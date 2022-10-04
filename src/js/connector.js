0678aec8ad9e30d24184a4e1e4e3215d97399531
Dirlandia404 <dirlandiaoliveira@alu.ufc.br>
2022-10-04 13:47:33 -0300

    add load card

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,6 +1,13 @@
 console.log("Hello world");
 window.TrelloPowerUp.initialize({
   "attachment-sections": function (t, options) {
-    return [];
+    return t.card("all").then(function (card) {
+      console.log("card");
+      return [
+        {
+          text: card.idShort,
+        },
+      ];
+    });
   },
 });
