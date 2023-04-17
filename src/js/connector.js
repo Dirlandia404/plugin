21a5f9d7ebd86abaffa0283f804d5e392409cf68
Dirlandia404 <dirlandiaoliveira@alu.ufc.br>
2023-04-17 16:07:29 -0300

    teste

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,13 +1,18 @@
 console.log("Hello world");
 window.TrelloPowerUp.initialize({
   "attachment-sections": function (t, options) {
-    return t.card("all").then(function (card) {
+    return t.card("coordenadas").then(function (card) {
       console.log("card");
-      return [
-        {
-          text: card.idShort,
-        },
-      ];
+      if (card.coordenadas) {
+        const (latitude, longitude) = card.coordenadas
+        fetch("https://api.meteomatics.com/")
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (whead) {});
+          console.log("teste push");
+      }
+      return [];
     });
   },
 });
